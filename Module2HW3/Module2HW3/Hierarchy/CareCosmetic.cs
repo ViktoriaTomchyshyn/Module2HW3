705b2774bfe06682d31039399034383a5f97bcb5
ViktoriaTomchyshyn <viktoriia.tomchyshyn@gmail.com>
2022-11-03 14:34:48 +0300

    Initial commit

---
+++ Module2HW3/Module2HW3/Hierarchy/CareCosmetic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2HW3
+{
+    public class CareCosmetic : Cosmetic, ICare
+    {
+        public CareCosmetic(int capacity, string name, float price, string purpose)
+            : base(capacity, name, price, purpose)
+        {
+        }
+
+        public string Care()
+        {
+            return "I`m caring about your body";
+        }
+    }
+}
---
+++ Module2HW3/Module2HW3/Hierarchy/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2HW3
+{
+    public class Product : Container, IComparable<Product>
+    {
+        public Product()
+            : base()
+        {
+            Name = string.Empty;
+            Price = 0;
+        }
+
+        public Product(int capacity, string name, float price)
+            : base(capacity)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+        public float Price { get; set; }
+
+        public int CompareTo(Product other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+
+            return Price.CompareTo(other.Price);
+        }
+    }
+}
